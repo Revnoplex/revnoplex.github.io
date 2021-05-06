038966d7e4ad04de7b399008e360b28ac36a8fae
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2021-05-06 12:26:58 +1000

    update footer link

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 document.write('\
 \
 <footer style="text-align: center;">\
-<p>Copyright © Revnoplex 2019-2021 <a href="https://github.com/revnoplex/revnoplex.github.io">Source Code</a></p>\
+<p>Copyright © Revnoplex 2019-2021 <a class="footer-link" href="https://github.com/revnoplex/revnoplex.github.io">Source Code</a></p>\
 </footer>')

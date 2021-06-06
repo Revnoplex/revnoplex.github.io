4ab822a9ff072424424d8853eb2d67ae3baa8774
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2021-06-06 18:00:44 +1000

    added download centre

--- title.js
+++ title.js
@@ -2,12 +2,12 @@ document.write('\
 \
 <h2 class="main-title">\
 <img src="/media/images/default-large.png" alt="main logo" width="32" height="32" usemap="#workmap"> Revnoplex \
-<span class="title-links"><a class="t-links" href="https://www.youtube.com/channel/UC1VSDiiRQZRTbxNvWhIrJfw"><img class="t-logos" src="/media/images/yt_logo.svg" alt="youtube-logo">Youtube</a>    \
+<span class="title-links"><a class="t-links" href="/downloads">Download Center</a>\
+<a class="t-links" href="https://www.youtube.com/channel/UC1VSDiiRQZRTbxNvWhIrJfw"><img class="t-logos" src="/media/images/yt_logo.svg" alt="youtube-logo">Youtube</a>    \
 <a class="t-links" href="https://www.twitter.com/revnoplex"><img class="t-logos" src="/media/images/twitter_logo.svg" alt="twitter-logo">Twitter</a>    \
 <a class="t-links" href="https://discord.com/oauth2/authorize?client_id=711582152940060859&permissions=4294967295&redirect_uri=https%3A%2F%2Frevnoplex.github.io%2Fadded-bot&response_type=code&scope=bot%20guilds%20applications.commands"><img class="t-logos" src="/media/images/discord_logo.svg" alt="discord-logo">Discord Bot</a>    \
 <a class="t-links" href="https://twitch.tv/Revnoplex"><img class="t-logos" src="/media/images/twitch_logo.svg" alt="twitch-logo">Twitch</a>    \
-<a class="t-links" href="https://github.com/revnoplex"><img class="t-logos" src="/media/images/github_logo.svg" alt="github-logo">Github</a>    \
-<a class="t-links" href="/downloads">Download Center</span></h2>\
+<a class="t-links" href="https://github.com/revnoplex"><img class="t-logos" src="/media/images/github_logo.svg" alt="github-logo">Github</a>    </span></h2>\
 <map name="workmap">\
   <area shape="default" coords="0,0,0,0" alt="revnoplex" href="/">\
 </map>');

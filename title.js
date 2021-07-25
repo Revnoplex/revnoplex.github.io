823b9c54ad7f8063dade56b59e867de58a5a6bc7
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2021-07-26 00:06:27 +1000

    Update title.js

--- title.js
+++ title.js
@@ -3,11 +3,11 @@ document.write('\
 <h2 class="main-title">\
 <img src="/media/images/default-large.png" alt="main logo" width="32" height="32" usemap="#workmap"> Revnoplex \
 <span class="title-links"><a class="t-links" href="/downloads">Download Center</a>\
-<a class="t-links" href="https://www.youtube.com/channel/UC1VSDiiRQZRTbxNvWhIrJfw"><img class="t-logos" src="/media/images/yt_logo.svg" alt="youtube-logo">Youtube</a>    \
-<a class="t-links" href="https://www.twitter.com/revnoplex"><img class="t-logos" src="/media/images/twitter_logo.svg" alt="twitter-logo">Twitter</a>    \
-<a class="t-links" href="/bot"><img class="t-logos" src="/media/images/discord_logo.svg" alt="discord-logo">Discord Bot</a>    \
-<a class="t-links" href="https://twitch.tv/Revnoplex"><img class="t-logos" src="/media/images/twitch_logo.svg" alt="twitch-logo">Twitch</a>    \
-<a class="t-links" href="https://github.com/revnoplex"><img class="t-logos" src="/media/images/github_logo.svg" alt="github-logo">Github</a>    </span></h2>\
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.youtube.com/channel/UC1VSDiiRQZRTbxNvWhIrJfw"><img class="t-logos" src="/media/images/yt_logo.svg" alt="youtube-logo">Youtube</a>    \
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.twitter.com/revnoplex"><img class="t-logos" src="/media/images/twitter_logo.svg" alt="twitter-logo">Twitter</a>    \
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="/bot"><img class="t-logos" src="/media/images/discord_logo.svg" alt="discord-logo">Discord Bot</a>    \
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://twitch.tv/Revnoplex"><img class="t-logos" src="/media/images/twitch_logo.svg" alt="twitch-logo">Twitch</a>    \
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://github.com/revnoplex"><img class="t-logos" src="/media/images/github_logo.svg" alt="github-logo">Github</a>    </span></h2>\
 <map name="workmap">\
   <area shape="default" coords="0,0,0,0" alt="revnoplex" href="/">\
 </map>');

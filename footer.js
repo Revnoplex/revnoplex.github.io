138283121f22b5c0cad1b9845d1e188736867c15
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2021-08-06 20:36:31 +1000

    fixed button links

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 document.write('\
 \
 <footer style="text-align: center;">\
-<p>Copyright © Revnoplex 2019-2021 <a class="f-link" target="_blank" rel="noreferrer noopener" href="https://github.com/revnoplex/revnoplex.github.io">Source Code</a></p>\
+<p>Copyright © Revnoplex 2019-2021 <a target="_blank" rel="noreferrer noopener" href="https://github.com/revnoplex/revnoplex.github.io">Source Code</a></p>\
 </footer>')

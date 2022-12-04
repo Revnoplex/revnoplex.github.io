2aa1ee765aec34cd5ce144f16554296b35ea32ef
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2022-12-04 13:25:00 +1100

    test new script again

--- bot/checker.js
+++ bot/checker.js
@@ -1,9 +1,13 @@
+var clicked = '<a id="addBotLink" class="btn" target="_blank" rel="noreferrer noopener" href="/redirects/revnobot">Add Bot</a>'
+var unclicked = '<p id="addBotLink">Button Disabled</p>'
 var terms = document.getElementById('terms');
 var addBotLink = document.getElementById('addBotLink');
+var addBot = document.getElementById('addBot')
+console.log(addBot);
 terms.onchange = function() {
     if (this.checked) {
-        addBotLink.href = "/redirects/revnobot"
+        addBot.innerHTML=clicked
     } else {
-        addBotLink.href = ""
-    }
+        addBot.innerHTML=unclicked
+    };
 };

da87a0c7ec1ddffa2eca2a4de2a41fff2641eff0
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2023-01-02 20:59:22 +1030

    update copyright year

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,5 +1,5 @@
 document.write('\
 \
 <footer style="text-align: center;">\
-<p>Copyright © Revnoplex 2019-2022 <a target="_blank" rel="noreferrer noopener" href="https://github.com/revnoplex/revnoplex.github.io">Source Code</a></p>\
+<p>Copyright © Revnoplex 2021-2023<a target="_blank" rel="noreferrer noopener" href="https://github.com/revnoplex/revnoplex.github.io">Source Code</a></p>\
 </footer>')

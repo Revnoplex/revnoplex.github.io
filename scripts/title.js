e74171ef5b0b73aa08e24ce6666cc9daf9bb23c4
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2023-07-22 22:02:17 +1000

    - Changed the /bot path to /revnobot and /bot is now a redirect to it - Moved terms of use and data collection policy to separate pages - Added new discord logo to "Discord Bot" button - Made <noscript> text easier to see - Made rickroll work with js disabled using <noscript>

--- scripts/title.js
+++ scripts/title.js
@@ -8,9 +8,9 @@ document.write('\
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.youtube.com/channel/UC1VSDiiRQZRTbxNvWhIrJfw"><img class="t-logos" src="/media/images/yt-logo.svg" alt="youtube-logo">Youtube</a>    \
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.twitter.com/revnoplex"><img class="t-logos" src="/media/images/twitter-logo.svg" alt="twitter-logo">Twitter</a>    \
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.reddit.com/user/C468"><img class="t-logos" src="/media/images/reddit-logo.svg" alt="reddit-logo">Reddit</a>    \
-<a class="t-links" href="/bot"><img class="t-logos" src="/media/images/discord-logo.svg" alt="discord-logo">Discord Bot</a>    \
-<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://twitch.tv/Revnoplex"><img class="t-logos" src="/media/images/twitch-logo.svg" alt="twitch-logo">Twitch</a>    \
-<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://github.com/revnoplex"><img class="t-logos" src="/media/images/github-logo.svg" alt="github-logo">Github</a>   \
+<a class="t-links" href="/revnobot"><img class="t-logos" src="/media/images/discord-logo.svg" alt="discord-logo"> Discord Bot</a>    \
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://twitch.tv/Revnoplex"><img class="t-logos" src="/media/images/twitch-logo.svg" alt="twitch-logo"> Twitch</a>    \
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://github.com/revnoplex"><img class="t-logos" src="/media/images/github-logo.svg" alt="github-logo"> Github</a>   \
  </span></h2>\
 <map name="workmap">\
   <area shape="default" coords="0,0,0,0" alt="revnoplex" href="/">\

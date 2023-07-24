ca9f605259e248132e96c5c65f56529a587e1294
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2023-07-24 21:55:25 +1000

    Add mastodon link for verifying website

--- scripts/title.js
+++ scripts/title.js
@@ -3,7 +3,7 @@ document.write('\
 <h2 class="main-title">\
 <img src="/media/images/default-black.png" alt="main logo" width="32" height="32" usemap="#workmap"> <a class="main-title-text" href="/">Revnoplex</a> \
 <span class="title-links">\
-<a class="t-links" href="/downloads">Download Center</a>\
+<a class="t-links" href="/downloads">Downloads</a>\
 <a class="t-links" href="/fun">Funny</a>\
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.youtube.com/channel/UC1VSDiiRQZRTbxNvWhIrJfw"><img class="t-logos" src="/media/images/yt-logo.svg" alt="youtube-logo">Youtube</a>    \
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.twitter.com/revnoplex"><img class="t-logos" src="/media/images/twitter-logo.svg" alt="twitter-logo">Twitter</a>    \
@@ -11,6 +11,7 @@ document.write('\
 <a class="t-links" href="/revnobot"><img class="t-logos" src="/media/images/discord-logo.svg" alt="discord-logo"> Discord Bot</a>    \
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://twitch.tv/Revnoplex"><img class="t-logos" src="/media/images/twitch-logo.svg" alt="twitch-logo"> Twitch</a>    \
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://github.com/revnoplex"><img class="t-logos" src="/media/images/github-logo.svg" alt="github-logo"> Github</a>   \
+<a class="t-links" target="_blank" rel="noreferrer noopener me" href="https://mastodon.social/@Revnoplex"><img class="t-logos" src="/media/images/mastodon-logo.svg" alt="mastodon-logo"> Mastodon</a> \
  </span></h2>\
 <map name="workmap">\
   <area shape="default" coords="0,0,0,0" alt="revnoplex" href="/">\

57b43ac9d12d1b817458f2bc02b49fabfbc1343e
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2024-02-21 23:15:47 +1100

    Added webring and refactored footer script

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,5 +1,3 @@
-document.write('\
-\
-<footer style="text-align: center;">\
-<p>Copyright © Revnoplex 2021-2024</p>\
-</footer>')
+var footerContent = '<footer style="text-align: center;"><p>Copyright © Revnoplex 2021-2024</p></footer> '
+var foot = document.getElementById('foot');
+foot.innerHTML=footerContent

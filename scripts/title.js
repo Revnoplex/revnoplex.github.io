fc88a50ad0f2a1f2662bd17505cd5ed2c5e40a67
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2024-04-27 21:30:09 +1000

    new button styling

--- scripts/title.js
+++ scripts/title.js
@@ -18,7 +18,7 @@ document.write('\
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.youtube.com/channel/UC1VSDiiRQZRTbxNvWhIrJfw"><img class="t-logos" src="/media/images/yt-logo.svg" alt="youtube-logo">Youtube</a>    \
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.twitter.com/revnoplex"><img class="t-logos" src="/media/images/twitter-logo.svg" alt="twitter-logo">Twitter</a>    \
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.reddit.com/user/C468"><img class="t-logos" src="/media/images/reddit-logo.svg" alt="reddit-logo">Reddit</a>    \
-<a class="t-links" href="/revnobot"><img class="t-logos" src="/media/images/discord-logo.svg" alt="discord-logo"> Discord Bot</a>    \
+<a class="t-links" href="/revnobot"><img class="t-logos" src="/media/images/revnobot.png" alt="discord-logo"> Discord Bot</a>    \
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://twitch.tv/Revnoplex"><img class="t-logos" src="/media/images/twitch-logo.svg" alt="twitch-logo"> Twitch</a>    \
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://github.com/revnoplex"><img class="t-logos" src="/media/images/github-logo.svg" alt="github-logo"> Github</a>   \
 <a class="t-links" target="_blank" rel="noreferrer noopener me" href="https://mastodon.social/@Revnoplex"><img class="t-logos" src="/media/images/mastodon-logo.svg" alt="mastodon-logo"> Mastodon</a> \

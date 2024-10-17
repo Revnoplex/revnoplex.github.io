f77ff7f31397af49d9ee5febc018eaea6627a67c
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2024-10-17 19:58:10 +1100

    Update bluesky handle

--- scripts/title.js
+++ scripts/title.js
@@ -22,7 +22,7 @@ document.write('\
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://www.reddit.com/user/C468"><img class="t-logos" src="/media/images/reddit-logo.svg" alt="reddit-logo">Reddit</a>    \
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://twitch.tv/Revnoplex"><img class="t-logos" src="/media/images/twitch-logo.svg" alt="twitch-logo"> Twitch</a>    \
 <a class="t-links" target="_blank" rel="noreferrer noopener" href="https://github.com/revnoplex"><img class="t-logos" src="/media/images/github-logo.svg" alt="github-logo"> Github</a>   \
-<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://bsky.app/profile/revnoplex.bsky.social"><img class="t-logos" src="/media/images/bluesky-logo.svg" alt="bluesky-logo">Bluesky</a>    \
+<a class="t-links" target="_blank" rel="noreferrer noopener" href="https://bsky.app/profile/revnoplex.xyz"><img class="t-logos" src="/media/images/bluesky-logo.svg" alt="bluesky-logo">Bluesky</a>    \
  </span></h2>\
 <map name="workmap">\
   <area shape="default" coords="0,0,0,0" alt="revnoplex" href="/">\

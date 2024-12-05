76baa217d9387fcedaf8f135c2f328b3001d7494
Revnoplex <62947003+Revnoplex@users.noreply.github.com>
2024-12-05 22:25:46 +1100

    added email

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,3 +1,3 @@
-var footerContent = '<footer style="text-align: center;"><p>Copyright © Revnoplex 2021-2024</p></footer> '
+var footerContent = '<footer style="text-align: center;"><p>Copyright © Revnoplex 2021-2024</p><p>Enquiries: <a style="color: #ffffff" href="mailto:webmaster@revnoplex.xyz">webmaster@revnoplex.xyz</a></p></footer> '
 var foot = document.getElementById('foot');
 foot.innerHTML=footerContent
